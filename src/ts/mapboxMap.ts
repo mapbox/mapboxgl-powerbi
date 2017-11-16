d5a707e5077f36e56f805a73e82fc328208d2b36
szilardhuber <szilard.huber@gmail.com>
2017-11-16 14:59:47 +0100

    Some further refactoring

--- src/ts/mapboxMap.ts
+++ src/ts/mapboxMap.ts
@@ -17,11 +17,52 @@ module powerbi.extensibility.visual {
         }
     }
 
+    function onUpdate(map, mapboxData, dataLayer) {
+        if (map.getSource('data')) {
+            let source : any = map.getSource('data');
+            source.setData( turf.featureCollection(mapboxData.features));
+            map.removeLayer('data');
+            map.addLayer(dataLayer);
+        }
+        else {
+            map.addSource('data', {
+                type: "geojson", 
+                data: turf.featureCollection(mapboxData.features),
+                buffer: 10
+            });
+
+            mapboxUtils.addBuildings(map);
+
+            map.addLayer(dataLayer, 'waterway-label');
+        }
+
+        let bounds : any = turf.bbox(turf.featureCollection(mapboxData.features));
+        bounds = bounds.map( bound => {
+            if (bound < -90) {
+                return -90;
+            }
+            if (bound > 90) {
+                return 90;
+            }
+            return bound;
+        });
+
+        map.easeTo( {
+            duration: 500,
+            pitch: 0,
+            bearing: 0
+        });
+        map.fitBounds(bounds, {
+            padding: 25
+        });
+        return true;
+    }
+
+
     export class MapboxMap implements IVisual {
         private map: mapboxgl.Map;
         private mapDiv: HTMLDivElement;
         private host: IVisualHost;
-        private firstRun: boolean = true;
         private mapboxData: MapboxData;
         private settings: MapboxSettings;
         private dataLayer: mapboxgl.Layer;
@@ -48,12 +89,6 @@ module powerbi.extensibility.visual {
             //Map initialization    
             this.mapDiv = document.createElement('div');
             this.mapDiv.className = 'map';
-            this.mapDiv.style.position = "absolute";
-            this.mapDiv.style.top = "0";
-            this.mapDiv.style.bottom ="0";
-            this.mapDiv.style.left ="0";
-            this.mapDiv.style.width = "100%";
-            this.mapDiv.style.overflow = 'visible';
             options.element.appendChild(this.mapDiv);
             
             let mapLegend: HTMLElement;
@@ -66,27 +101,6 @@ module powerbi.extensibility.visual {
                 closeButton: false,
                 closeOnClick: false
             });
-        }
-
-        @logExceptions()
-        public update(options: VisualUpdateOptions) {
-            let styleChanged = false;
-            var _this = this
-            //Only run this step if there are lat/long values to parse
-            if (options.dataViews[0].metadata.columns.length < 2) { 
-                this.firstRun = false;
-                return 
-            };
-            const dataView: DataView = options.dataViews[0];
-            this.mapboxData  = mapboxConverter.convert(dataView, this.host);
-            this.settings = MapboxSettings.parse<MapboxSettings>(dataView);
-
-
-            if (!this.settings.api.accessToken) {
-                return;
-            }
-            mapboxgl.accessToken = this.settings.api.accessToken;
-
 
             const mapOptions = {
                 container: this.mapDiv,
@@ -95,99 +109,59 @@ module powerbi.extensibility.visual {
             }
 
             //If the map container doesnt exist yet, create it
-            if (this.map === undefined ) {
-                this.map = new mapboxgl.Map(mapOptions);
-                this.map.addControl(new mapboxgl.NavigationControl());
-                this.map.on('style.load', runload);
-                mapboxUtils.addPopup(this.map, this.popup);
-                mapboxUtils.addClick(this.map);
+            this.map = new mapboxgl.Map(mapOptions);
+            this.map.addControl(new mapboxgl.NavigationControl());
+            this.map.on('style.load', () => onUpdate(this.map, this.mapboxData, this.dataLayer));
+            this.map.on('load', () => onUpdate(this.map, this.mapboxData, this.dataLayer));
+            mapboxUtils.addPopup(this.map, this.popup);
+            mapboxUtils.addClick(this.map);
+        }
+
+        @logExceptions()
+        public update(options: VisualUpdateOptions) {
+            const dataView: DataView = options.dataViews[0];
+            this.settings = MapboxSettings.parse<MapboxSettings>(dataView);
+            
+            // Only run this step if there are lat/long values to parse
+            // and accessToken is set in options
+            if (options.dataViews[0].metadata.columns.length < 2 || !this.settings.api.accessToken) { 
+                return 
+            };
+
+            this.mapboxData  = mapboxConverter.convert(dataView, this.host);
+
+            if (mapboxgl.accessToken != this.settings.api.accessToken) {
+                mapboxgl.accessToken = this.settings.api.accessToken;
             }
-            const layerType = this.settings.api.layerType;
 
             const layer = mapboxUtils.decorateLayer({
                 id: 'data',
                 source: 'data',
-                type: layerType
+                type: this.settings.api.layerType,
             }, dataView.table.columns, this.mapboxData.maxSize)
+
             if (this.dataLayer != layer ) {
                 this.dataLayer = layer;
             }
 
-
+            let styleChanged = false;
             if (this.mapStyle != this.settings.api.style) {
                 this.mapStyle = this.settings.api.style;
                 styleChanged = true;
                 this.map.setStyle(this.mapStyle);
             }
 
-
-            function onUpdate() {
-                if (_this.map.getSource('data')) {
-                    let source : any = _this.map.getSource('data');
-                    source.setData( turf.featureCollection(_this.mapboxData.features));
-                    _this.map.removeLayer('data');
-                    _this.map.addLayer(_this.dataLayer);
-                }
-                else {
-                    _this.map.addSource('data', {
-                        type: "geojson", 
-                        data: turf.featureCollection(_this.mapboxData.features),
-                        buffer: 10
-                    });
-
-                    mapboxUtils.addBuildings(_this.map);
-
-                    _this.map.addLayer(_this.dataLayer, 'waterway-label');
+            // If the map is loaded and style has not changed in this update
+            // then we should update right now.
+            if (this.map.loaded && !styleChanged) {
+                onUpdate(this.map, this.mapboxData, this.dataLayer);
             }
         }
-
-
-        function runload() {
-            onUpdate();
-
-            let bounds : any = turf.bbox(turf.featureCollection(_this.mapboxData.features));
-            bounds = bounds.map( bound => {
-                if (bound < -90) {
-                    return -90;
-                }
-                if (bound > 90) {
-                    return 90;
-                }
-                return bound;
-            });
-
-            _this.map.easeTo( {
-                duration: 500,
-                pitch: 0,
-                bearing: 0
-            });
-            _this.map.fitBounds(bounds, {
-                padding: 25
-            });
-            _this.firstRun = false;
-            return true;
-        }
-
-        // If running update for the first time, wait for the load event
-        if (_this.firstRun) {
-            _this.map.once('load', runload);
-        }
-        else {
-            //If refreshing the map, update the map if it's already fully rendered
-            if (_this.map.loaded && !styleChanged) {
-                runload();
-            }
-            else {
-                //If refreshing the map and existing data is still loading, update when finished loading
-                //_this.map.on('sourcedata', runload);
-            }  
-        }
-        
-    }
 
         @logExceptions()
         public destroy(): void {
             this.map.remove();
+            this.map = null;
         }
     }
 }
