3a30b3718c71e8be419e58ba563d4a92e867f051
Peter Bastyi <baspeti@gmail.com>
2018-03-29 22:44:42 +0200

    Toggle zoom pin

--- src/ts/autoZoomControl.ts
+++ src/ts/autoZoomControl.ts
@@ -4,14 +4,23 @@ module powerbi.extensibility.visual {
         private _map: mapboxgl.Map;
         private _container: HTMLElement;
         private _zoomPinButton: HTMLElement;
+        private _toggled: boolean;
 
         public onAdd(map) {
+            this._toggled = false;
             this._map = map;
             this._container = document.createElement('div');
-            this._container.className = 'mapboxgl-ctrl-group';
+            this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
             this._zoomPinButton = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin', 'Toggle auto zoom',
-                                                     () => this._map.zoomIn());
-            return this._zoomPinButton;
+                                                     () => {
+                                                        this._toggled = !this._toggled;
+                                                        let buttonClassName = 'mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin';
+                                                        if (this._toggled) {
+                                                            buttonClassName = 'mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin-toggled';
+                                                        }
+                                                        this._zoomPinButton.className = buttonClassName;
+                                                     });
+            return this._container;
         }
 
         public onRemove() {
@@ -20,11 +29,10 @@ module powerbi.extensibility.visual {
         }
 
         public getDefaultPosition() {
-            return 'top-left';
+            return 'top-right';
         }
 
         private _createButton(className: string, ariaLabel: string, fn: () => any) {
-
             const a = this._createElement('button', className, this._container);
             a.type = 'button';
             a.setAttribute('aria-label', ariaLabel);
