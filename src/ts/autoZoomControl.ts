e482173696151b65e400a8f3381a3d909cbbbe71
Peter Bastyi <baspeti@gmail.com>
2018-03-29 23:02:33 +0200

    Do not underscore private member names and functions

--- src/ts/autoZoomControl.ts
+++ src/ts/autoZoomControl.ts
@@ -1,31 +1,31 @@
 module powerbi.extensibility.visual {
 
     export class AutoZoomControl implements mapboxgl.IControl {
-        private _map: mapboxgl.Map;
-        private _container: HTMLElement;
-        private _zoomPinButton: HTMLElement;
-        private _toggled: boolean;
+        private map: mapboxgl.Map;
+        private container: HTMLElement;
+        private zoomPinButton: HTMLElement;
+        private toggled: boolean;
 
         public onAdd(map) {
-            this._toggled = false;
-            this._map = map;
-            this._container = document.createElement('div');
-            this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-            this._zoomPinButton = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin', 'Toggle auto zoom',
+            this.toggled = false;
+            this.map = map;
+            this.container = document.createElement('div');
+            this.container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+            this.zoomPinButton = this.createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin', 'Toggle auto zoom',
                                                      () => {
-                                                        this._toggled = !this._toggled;
+                                                        this.toggled = !this.toggled;
                                                         let buttonClassName = 'mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin';
-                                                        if (this._toggled) {
+                                                        if (this.toggled) {
                                                             buttonClassName = 'mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-pin-toggled';
                                                         }
-                                                        this._zoomPinButton.className = buttonClassName;
+                                                        this.zoomPinButton.className = buttonClassName;
                                                      });
-            return this._container;
+            return this.container;
         }
 
         public onRemove() {
-            this._container.parentNode.removeChild(this._container);
-            this._map = undefined;
+            this.container.parentNode.removeChild(this.container);
+            this.map = undefined;
         }
 
         public getDefaultPosition() {
@@ -33,18 +33,18 @@ module powerbi.extensibility.visual {
         }
 
         public isPinned() {
-            return this._toggled;
+            return this.toggled;
         }
 
-        private _createButton(className: string, ariaLabel: string, fn: () => any) {
-            const a = this._createElement('button', className, this._container);
+        private createButton(className: string, ariaLabel: string, fn: () => any) {
+            const a = this.createElement('button', className, this.container);
             a.type = 'button';
             a.setAttribute('aria-label', ariaLabel);
             a.addEventListener('click', fn);
             return a;
         }
 
-        private _createElement = function (tagName: any, className?: string, container?: HTMLElement) {
+        private createElement = function (tagName: any, className?: string, container?: HTMLElement) {
             const el = window.document.createElement(tagName);
             if (className) el.className = className;
             if (container) container.appendChild(el);
