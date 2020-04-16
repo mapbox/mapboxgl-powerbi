a175326188d9f346053c75df5ef48ae024bee0aa
Szilárd Huber <szilard.huber@gmail.com>
2020-04-16 17:56:44 +0200

    Reorganize source files

--- src/js/mapboxglAdaptor.js
+++
@@ -1,7 +0,0 @@
-const mapboxgl = window['mapboxgl'];
-const chroma = window['chroma'];
-const turf = window['turf'];
-const supercluster = window['supercluster'];
-const MapboxDraw = window['MapboxDraw'];
-const MapboxGeocoder = window['MapboxGeocoder'];
-const fastDeepEqual = window['fastDeepEqual']
