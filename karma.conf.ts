8ac1ed18296e01c34ee608f062f1c6c32f9e848c
Mátyás Budavári <budavariam@gmail.com>
2020-04-23 17:16:03 +0200

    Update unit tests to use new module syntax
    Update karma config based on this doc:
    https://docs.microsoft.com/en-us/power-bi/developer/visuals/unit-tests-introduction

---
+++ karma.conf.ts
@@ -0,0 +1,102 @@
+
+"use strict";
+
+const webpackConfig = require("./test.webpack.config.js");
+const tsconfig = require("./tsconfig.json"); // or separate test.tsconfig.json
+const path = require("path");
+
+const testRecursivePath = "test/visualTest.ts";
+const srcOriginalRecursivePath = "src/**/*.ts";
+const coverageFolder = "coverage";
+
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
+import { Config, ConfigOptions } from "karma";
+
+module.exports = (config: Config) => {
+    config.set(<ConfigOptions>{
+        mode: "development",
+        client: {
+            accessToken: process.env.MAPBOX_TOKEN || "",
+        },
+        browserNoActivityTimeout: 100000,
+        browsers: ["ChromeHeadless"], // or Chrome to use locally installed Chrome browser
+        colors: true,
+        frameworks: ["jasmine"],
+        reporters: [
+            "progress",
+            "junit",
+            "coverage-istanbul"
+        ],
+        junitReporter: {
+            outputDir: path.join(__dirname, coverageFolder),
+            outputFile: "TESTS-report.xml",
+            useBrowserName: false
+        },
+        singleRun: true,
+        plugins: [
+            "karma-coverage",
+            "karma-typescript",
+            "karma-webpack",
+            "karma-jasmine",
+            "karma-sourcemap-loader",
+            "karma-chrome-launcher",
+            "karma-junit-reporter",
+            "karma-coverage-istanbul-reporter"
+        ],
+        files: [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/jasmine-jquery/lib/jasmine-jquery.js",
+            {
+                pattern: './capabilities.json',
+                watched: false,
+                served: true,
+                included: false
+            },
+            testRecursivePath,
+            {
+                pattern: srcOriginalRecursivePath,
+                included: false,
+                served: true
+            }
+        ],
+        preprocessors: {
+            [testRecursivePath]: ["webpack", "coverage"]
+        },
+        typescriptPreprocessor: {
+            options: tsconfig.compilerOptions
+        },
+        coverageIstanbulReporter: {
+            reports: ["html", "lcovonly", "text-summary", "cobertura"],
+            dir: path.join(__dirname, coverageFolder),
+            'report-config': {
+                html: {
+                    subdir: 'html-report'
+                }
+            },
+            combineBrowserReports: true,
+            fixWebpackSourcePaths: true,
+            verbose: false
+        },
+        coverageReporter: {
+            dir: path.join(__dirname, coverageFolder),
+            reporters: [
+                // reporters not supporting the `file` property
+                { type: 'html', subdir: 'html-report' },
+                { type: 'lcov', subdir: 'lcov' },
+                // reporters supporting the `file` property, use `subdir` to directly
+                // output them in the `dir` directory
+                { type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml' },
+                { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+                { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+            ]
+        },
+        mime: {
+            "text/x-typescript": ["ts", "tsx"]
+        },
+        webpack: webpackConfig,
+        webpackMiddleware: {
+            stats: "errors-only"
+        }
+    });
+};
--- test/visualTest.ts
+++ test/visualTest.ts
@@ -1,138 +1,142 @@
-/// <reference path="_references.ts" />
+import { MapboxData } from "./visualData"
+import { MapboxMap as Visual } from "../src/visual" 
+import { MapboxChartBuilder } from "./visualBuilder"
+import { MockILocale, MockIAllowInteractions, MockITooltipService, MockIColorPalette, MockISelectionManager, MockIVisualHost } from "powerbi-visuals-utils-testutils"
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualHost = powerbiVisualsApi.extensibility.visual.IVisualHost;
+import ISelectionManager = powerbiVisualsApi.extensibility.ISelectionManager;
+import ITooltipService = powerbiVisualsApi.extensibility.ITooltipService;
+import IColorPalette = powerbiVisualsApi.extensibility.ISandboxExtendedColorPalette
+import DataView = powerbiVisualsApi.DataView
+declare const __karma__: any;
 
-module powerbi.extensibility.visual.test {
+describe("Mapbox visual", () => {
 
-    import MapboxData = powerbi.extensibility.visual.test.MapboxData;
-    import MapboxChartBuilder = powerbi.extensibility.visual.test.MapboxChartBuilder;
+    // define global spec variables here
+    let host: IVisualHost;
+    let target: HTMLElement;
+    let palette: IColorPalette;
+    let selectionManager: ISelectionManager;
+    let tooltipService: ITooltipService;
+    let locale: MockILocale;
+    let allowInteractions: MockIAllowInteractions;
+    let visualBuilder: MapboxChartBuilder;
+    let dataViewBuilder: MapboxData;
+    let dataView: DataView;
 
-    describe("Mapbox visual", () => {
+    beforeEach(() => {
 
-        // define global spec variables here
-        let host: IVisualHost;
-        let target: HTMLElement;
-        let palette: IColorPalette;
-        let selectionManager: ISelectionManager;
-        let tooltipService: ITooltipService;
-        let locale: MockILocale;
-        let allowInteractions: MockIAllowInteractions;
-        let visualBuilder: MapboxChartBuilder;
-        let dataViewBuilder: MapboxData;
-        let dataView: DataView;
+        // mock constructor input variables
+        target = document.createElement("div");
+        palette = new MockIColorPalette();
+        selectionManager = new MockISelectionManager();
+        tooltipService = new MockITooltipService();
+        locale = new MockILocale({ "en": "en-US" });
+        allowInteractions = new MockIAllowInteractions(true);
+        host = new MockIVisualHost(palette, selectionManager, tooltipService, locale, allowInteractions);
 
-        beforeEach(() => {
-
-            // mock constructor input variables
-            target = document.createElement("div");
-            palette = new MockIColorPalette();
-            selectionManager = new MockISelectionManager();
-            tooltipService = new MockITooltipService();
-            locale = new MockILocale({ "en": "en-US" });
-            allowInteractions = new MockIAllowInteractions(true);
-            host = new MockIVisualHost(palette, selectionManager, tooltipService, locale, allowInteractions);
-
-            visualBuilder = new MapboxChartBuilder(1920, 1080);
-            dataViewBuilder = new MapboxData();
-            dataView = dataViewBuilder.getDataView();
-        });
-
-        describe("the constructor method", () => {
-
-            it("must create a visual without errors", () => {
-
-                // create the visual for testing
-                const visual = new Visual({ element: target, host: host });
-
-                // ensure it exists
-                expect(visual).toBeDefined();
-
-            });
-        });
-
-        describe("the mapbox canvas", () => {
-            beforeEach(() => {
-                if (!__karma__.config.accessToken.length) {
-                    fail("MAPBOX_TOKEN environment variable is not set. Please see README.md")
-                    return
-                }
-                dataView.metadata.objects = {
-                    api: {
-                        accessToken: __karma__.config.accessToken,
-                        style: 'mapbox://styles/mapbox/light-v9?optimize=true',
-                    },
-                    circle: {
-                        show: false,
-                    },
-                    choropleth: {
-                        show: false,
-                    },
-                    cluster: {
-                        show: false,
-                    },
-                    heatmap: {
-                        show: false,
-                    },
-                }
-            })
-
-            it("should be able to display circle viz", (done) => {
-                dataView.metadata.objects.circle.show = true;
-                visualBuilder.updateRenderTimeout(dataView, () => {
-                    let errorText = ""
-                    if (visualBuilder.error.length > 0) {
-                        errorText = visualBuilder.error[0].innerText;
-                    }
-                    expect(errorText).toBe("", "The error element should be empty");
-                    expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
-                    visualBuilder.viz.on('updated', () => {
-                        done();
-                    });
-                });
-            })
-
-            it("should be able to display cluster viz", (done) => {
-                dataView.metadata.objects.cluster.show = true;
-                visualBuilder.updateRenderTimeout(dataView, () => {
-                    let errorText = ""
-                    if (visualBuilder.error.length > 0) {
-                        errorText = visualBuilder.error[0].innerText;
-                    }
-                    expect(errorText).toBe("", "The error element should be empty");
-                    expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
-                    visualBuilder.viz.on('updated', () => {
-                        done();
-                    });
-                });
-            })
-
-            it("should be able to display heatmap viz", (done) => {
-                dataView.metadata.objects.heatmap.show = true;
-                visualBuilder.updateRenderTimeout(dataView, () => {
-                    let errorText = ""
-                    if (visualBuilder.error.length > 0) {
-                        errorText = visualBuilder.error[0].innerText;
-                    }
-                    expect(errorText).toBe("", "The error element should be empty");
-                    expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
-                    visualBuilder.viz.on('updated', () => {
-                        done();
-                    });
-                });
-            })
-
-            it("should be able to display choropleth viz", (done) => {
-                dataView.metadata.objects.choropleth.show = true;
-                visualBuilder.updateRenderTimeout(dataView, () => {
-                    let errorText = ""
-                    if (visualBuilder.error.length > 0) {
-                        errorText = visualBuilder.error[0].innerText;
-                    }
-                    expect(errorText).toBe("", "The error element should be empty");
-                    expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
-                    visualBuilder.viz.on('updated', () => {
-                        done();
-                    });
-                });
-            })
-        })
+        visualBuilder = new MapboxChartBuilder(1920, 1080);
+        dataViewBuilder = new MapboxData();
+        dataView = dataViewBuilder.getDataView();
     });
-}
+
+    describe("the constructor method", () => {
+
+        it("must create a visual without errors", () => {
+
+            // create the visual for testing
+            const visual = new Visual({ element: target, host: host });
+
+            // ensure it exists
+            expect(visual).toBeDefined();
+
+        });
+    });
+
+    describe("the mapbox canvas", () => {
+        beforeEach(() => {
+            if (!__karma__.config.accessToken.length) {
+                fail("MAPBOX_TOKEN environment variable is not set. Please see README.md")
+                return
+            }
+            dataView.metadata.objects = {
+                api: {
+                    accessToken: __karma__.config.accessToken,
+                    style: 'mapbox://styles/mapbox/light-v9?optimize=true',
+                },
+                circle: {
+                    show: false,
+                },
+                choropleth: {
+                    show: false,
+                },
+                cluster: {
+                    show: false,
+                },
+                heatmap: {
+                    show: false,
+                },
+            }
+        })
+
+        it("should be able to display circle viz", (done) => {
+            dataView.metadata.objects.circle.show = true;
+            visualBuilder.updateRenderTimeout(dataView, () => {
+                let errorText = ""
+                if (visualBuilder.error.length > 0) {
+                    errorText = visualBuilder.error[0].innerText;
+                }
+                expect(errorText).toBe("", "The error element should be empty");
+                expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
+                visualBuilder.viz.on('updated', () => {
+                    done();
+                });
+            });
+        })
+
+        it("should be able to display cluster viz", (done) => {
+            dataView.metadata.objects.cluster.show = true;
+            visualBuilder.updateRenderTimeout(dataView, () => {
+                let errorText = ""
+                if (visualBuilder.error.length > 0) {
+                    errorText = visualBuilder.error[0].innerText;
+                }
+                expect(errorText).toBe("", "The error element should be empty");
+                expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
+                visualBuilder.viz.on('updated', () => {
+                    done();
+                });
+            });
+        })
+
+        it("should be able to display heatmap viz", (done) => {
+            dataView.metadata.objects.heatmap.show = true;
+            visualBuilder.updateRenderTimeout(dataView, () => {
+                let errorText = ""
+                if (visualBuilder.error.length > 0) {
+                    errorText = visualBuilder.error[0].innerText;
+                }
+                expect(errorText).toBe("", "The error element should be empty");
+                expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
+                visualBuilder.viz.on('updated', () => {
+                    done();
+                });
+            });
+        })
+
+        it("should be able to display choropleth viz", (done) => {
+            dataView.metadata.objects.choropleth.show = true;
+            visualBuilder.updateRenderTimeout(dataView, () => {
+                let errorText = ""
+                if (visualBuilder.error.length > 0) {
+                    errorText = visualBuilder.error[0].innerText;
+                }
+                expect(errorText).toBe("", "The error element should be empty");
+                expect(visualBuilder.map.length).toBe(1, "There should be a Mapbox canvas element on the page");
+                visualBuilder.viz.on('updated', () => {
+                    done();
+                });
+            });
+        })
+    })
+});
